971b73f41cc9c3a24b692a792a278e25d77f55ee
Ben Leung <benjamin.leung80@gmail.com>
2018-10-08 13:49:12 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
+// part 1
+// imagine you have the power of god in a programming world
+// you create a variable (think container) out of thin air
+
 var buttonNothing;
 var buttonHoverOver;
 var buttonClick;
 var imgCatInMug;
+
 var button2X;
 var button2Y;
 
 function preload()
 {
-	imgCatInMug = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/CatInMug.png');
+	// part 2
+	// now that you have created a variable it is time to put some 
+	// data into the variable, the code below does that
 
+	imgCatInMug = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/CatInMug.png');
 	buttonClick = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonClick.png');
 	buttonHoverOver = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonHoverOver.png');
-	buttonNothing = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonNothing.png');
+  	buttonNothing = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonNothing.png');
 }
 
 function setup()
@@ -20,7 +28,6 @@ function setup()
 
 	button2X = 1;
 	button2Y = 1;
-  
 }
 
 function draw()
@@ -28,20 +35,18 @@ function draw()
 	background(125,125,125)
 
 
-	// Example 1: easy to understand - Start
+	// part 3
+	// show the contents of the variable on the canvas
+	// image(imageVariable,x,y,length,height)
 
-	image(imgCatInMug,10,150);
-
-	// Example 1: easy to understand - End
-
-
+	image(imgCatInMug,10,150,1060/3,702/3);
 
 
-	// Example 2: less code but harder to understand - Start
-
-	strokeWeight(3);
 
 
+
+	// the code below shows you how you can
+	// use images to represent buttons
 
 	if (mouseX > button2X && mouseX < button2X + 200 && mouseY > button2Y && mouseY < button2Y+50)
 	{
@@ -55,9 +60,6 @@ function draw()
 	{
 		image(buttonNothing,button2X,button2Y);
 	}
-
-
-	// Example 2: less code but harder to understand - End
 
 
 }
